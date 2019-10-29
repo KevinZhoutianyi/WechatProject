1d41456cb164e4cc409e40fb138e0d104725294b
OnlyDrinkWater <kevinzhou2@live.cn>
2019-10-29 15:56:53 +0000

    wechat init

---
+++ wechatproject/pages/shouye/shouye.js
@@ -0,0 +1,121 @@
+// pages/shouye/shouye.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperImg:[
+      {src:"/images/banner.png"},
+      { src: "/images/banner.png"},
+      { src: "/images/banner.png"}
+    ],
+    
+    currentTab: 0,
+  },
+
+  bindInput: function (e) {
+    // this.setData({
+    //   inputValue: e.detail.value
+    // })
+
+    wx.navigateTo({
+      url: '../sousuo/sousuo'
+    })
+  },
+
+
+
+  swichNav: function (e) {
+
+    console.log(e);
+
+    var that = this;
+
+    if (this.data.currentTab === e.target.dataset.current) {
+
+      return false;
+
+    } else {
+
+      that.setData({
+
+        currentTab: e.target.dataset.current,
+
+      })
+
+    }
+
+  },
+
+  swiperChange: function (e) {
+
+    console.log(e);
+
+    this.setData({
+
+      currentTab: e.detail.current,
+
+    })
+
+
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
